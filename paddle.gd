f7f06ed8633fad823aaad338b7772957bf7ba3c3
Pataclop <escure.f@gmail.com>
2019-11-09 10:14:16 +0100

    generation random

--- paddle.gd
+++ paddle.gd
@@ -1,41 +1,31 @@
 extends Area2D
 export var ball_dir = 1
 
-var centerPosX=100
-var centerPosY=100
-var width = 0
-var heigth = 0
+
+var width = get_viewport_rect().size[0]
+var heigth = get_viewport_rect().size[1]
+
+var centerPosX=width/2
+var centerPosY=heigth/2
+
 var angle = 0.0
-var radius = 0
+var radius = width/5
 var center = Vector2(centerPosX,centerPosY)
 
+
 	
-func _draw():
-	draw_circle_arc(Vector2(radius,radius), radius, 0, 360, Color(1.0, 1.0, 1.0))
-	
-
-
-func draw_circle_arc(center, radius, angle_from, angle_to, color):
-    var nb_points = 32
-    var points_arc = PoolVector2Array()
-
-    for i in range(nb_points + 1):
-        var angle_point = deg2rad(angle_from + i * (angle_to-angle_from) / nb_points - 90)
-        points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
-
-    for index_point in range(nb_points):
-        draw_line(points_arc[index_point], points_arc[index_point + 1], color)
-	
-
 func _process(delta):
 	width = get_viewport_rect().size[0]
 	heigth = get_viewport_rect().size[1]
 	radius = width/15
 	var which = get_name()
-	position.x = centerPosX+radius*cos(angle)
-	position.y = centerPosY+radius*sin(angle)
+
+
+	
+	position.x = centerPosX+radius*cos(angle)-((0.2*100)*cos(angle))
+	position.y = centerPosY+radius*sin(angle)-((0.2*100)*sin(angle))#-size[1]/2
 	angle +=0.03
-	update()
+
 
 
 	
@@ -51,6 +41,7 @@ func _input(event):
 		centerPosX = event.position.x
 		centerPosY = event.position.y
 		center = Vector2(centerPosX,centerPosY)
+		
 		
 
 	
